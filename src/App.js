90a46322e0c9b31d7cddc312d52ae513bfe4b033
MHilhorst <m.j.hilhorst@students.uu.nl>
2020-10-21 14:59:55 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,74 +1,90 @@
-import React from 'react';
-import './App.css';
-import { Layout, Menu, Breadcrumb,List,Avatar,Tag,Typography} from 'antd'
+import React from "react";
+import "./App.css";
+import { Layout, List, Avatar, Tag, Spin } from "antd";
 
 const { Header, Content, Footer } = Layout;
-const { Text} = Typography
-
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.fetchItems = this.fetchItems.bind(this)
-    this.handleProducts = this.handleProducts.bind(this)
-    this.state = { loading: true,products:[]}
+  constructor(props) {
+    super(props);
+    this.fetchItems = this.fetchItems.bind(this);
+    this.handleProducts = this.handleProducts.bind(this);
+    this.state = { loading: true, products: [] };
   }
 
-  fetchItems =  async () => {
-    const response = await fetch('/.netlify/functions/index');
+  fetchItems = async () => {
+    const response = await fetch("/.netlify/functions/index");
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    return body
-  }
-
+    return body;
+  };
 
   handleProducts = async () => {
-    const products = await this.fetchItems()
-    return products.map(product => {
-      return {avatar:product.image,discount:product.discount, title:product.description,description:product.description,price:product.price}
-    })
-  }
-  async componentDidMount(){
-    const products = await this.handleProducts()
-    if(products){
-      this.setState({loading:false,products:products})
-      console.log(this.state.products)
-
+    const products = await this.fetchItems();
+    return products.map((product) => {
+      return {
+        avatar: product.image,
+        discount: product.discount,
+        title: product.description,
+        description: product.description,
+        price: product.price,
+      };
+    });
+  };
+  async componentDidMount() {
+    const products = await this.handleProducts();
+    if (products) {
+      this.setState({ loading: false, products: products });
     }
-
   }
-  render(){
-    return(
+  render() {
+    return (
       <Layout className="layout">
-      <Header>
-        <div className="logo" />
-      </Header>
-      <Content style={{ padding: '0 50px',margin:'32px 0' }}>
-        <div className="site-layout-content">
-        {this.state.products.length > 0 && <List
-    itemLayout="horizontal"
-    dataSource={this.state.products}
-    renderItem={item => (
-      <List.Item>
-        <List.Item.Meta
-        avatar={<Avatar src={item.avatar} size={75}/>}
-          title={<a href="https://ant.design">{item.title}</a>}
-          description={<>
-          {item.description}
-          <br />
-          <Tag style={{marginTop:8}} color={item.price.was ? `#f40` : `#87d068`}>{item.price.now}</Tag>{item.price.was ? <Tag color="#87d068">{item.price.was}</Tag> : null}
-          {item.discount ? <Tag style={{marginTop:8}} color="#ff7900">{item.discount.label}</Tag>: null}
-          </>
-          }
-        />
-      </List.Item>
-    )}
-    />}
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Coornhertstraat 66!!</Footer>
-    </Layout>
-    )
+        <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+          <div className="title">Coornhertstraat 66</div>
+        </Header>
+        <Content style={{ marginTop: 50 }}>
+          <div className="site-layout-content">
+            {this.state.loading && <Spin size="large" />}
+            {this.state.products.length > 0 && (
+              <List
+                itemLayout="horizontal"
+                dataSource={this.state.products}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.avatar} size={75} />}
+                      title={item.title}
+                      description={
+                        <>
+                          {item.description}
+                          <br />
+                          <Tag
+                            style={{ marginTop: 8 }}
+                            color={item.price.was ? `#f40` : `#87d068`}
+                          >
+                            {item.price.now}
+                          </Tag>
+                          {item.price.was ? (
+                            <Tag color="#87d068">{item.price.was}</Tag>
+                          ) : null}
+                          {item.discount ? (
+                            <Tag style={{ marginTop: 8 }} color="#ff7900">
+                              {item.discount.label}
+                            </Tag>
+                          ) : null}
+                        </>
+                      }
+                    />
+                  </List.Item>
+                )}
+              />
+            )}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Coornhertstraat 66!!</Footer>
+      </Layout>
+    );
   }
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,52 +1,61 @@
 // const ah = require('albert-heijn')
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
-class Product { 
-    constructor(product_id){
-        this.product_id = product_id
+class Product {
+  constructor(product_id) {
+    this.product_id = product_id;
 
-    
     this.get_product_info = async () => {
-        const response = await fetch(`https://www.ah.nl/service/rest/delegate?url=producten/product/${this.product_id}`)
-        const data = await response.json()
-        this.structure_data(data)
-    }
+      const response = await fetch(
+        `https://www.ah.nl/service/rest/delegate?url=producten/product/${this.product_id}`
+      );
+      const data = await response.json();
+      this.structure_data(data);
+    };
     this.structure_data = (raw_data) => {
-        const data = raw_data._embedded.lanes.filter(data => {
-            if(data.type =='ProductDetailLane'){
-                return data
-            }
-        })[0]._embedded.items[0]
-        this.discount = data._embedded.product.discount
-        this.id = data._embedded.product.id
-        this.price = data._embedded.product.priceLabel
-        this.description = data._embedded.product.description
-        this.image = data._embedded.product.images[0].link.href
-    }
+      const data = raw_data._embedded.lanes.filter((data) => {
+        if (data.type == "ProductDetailLane") {
+          return data;
+        }
+      })[0]._embedded.items[0];
+      this.discount = data._embedded.product.discount;
+      this.id = data._embedded.product.id;
+      this.price = data._embedded.product.priceLabel;
+      this.description = data._embedded.product.description;
+      this.image = data._embedded.product.images[0].link.href;
+    };
+  }
 }
-}
-const all_products = ['wi210145/heineken-premium-pilsener',
-'wi479884/sun-vaatwaspoeder-normaal',
-'wi58053/ah-olijfolie-mild',
-'wi65764/drogheria-4-seizoenen-peper',
-'wi65765/drogheria-middellandse-zeezout',
-'wi33693/ah-halfvolle-melk',
-'wi2770/karvan-cevitam-aardbei-siroop',
-'wi125035/ah-les-pains-boulogne-meergranen-heel'
-]
+const all_products = [
+  "wi210145/heineken-premium-pilsener",
+  "wi479884/sun-vaatwaspoeder-normaal",
+  "wi58053/ah-olijfolie-mild",
+  "wi65764/drogheria-4-seizoenen-peper",
+  "wi65765/drogheria-middellandse-zeezout",
+  "wi33693/ah-halfvolle-melk",
+  "wi2770/karvan-cevitam-aardbei-siroop",
+  "wi125035/ah-les-pains-boulogne-meergranen-heel",
+];
 
 const func = async (event, context) => {
-    const response = await Promise.all(all_products.map(async item => {
-        const new_product = new Product(item)
-        await new_product.get_product_info()
-        return {id:new_product.id,price:new_product.price,discount:new_product.discount,description:new_product.description,image:new_product.image}
-    }))
-    
-    return {
-        body:JSON.stringify(response),
-        statusCode:200
-    }
-}
+  const response = await Promise.all(
+    all_products.map(async (item) => {
+      const new_product = new Product(item);
+      await new_product.get_product_info();
+      return {
+        id: new_product.id,
+        price: new_product.price,
+        discount: new_product.discount,
+        description: new_product.description,
+        image: new_product.image,
+      };
+    })
+  );
 
-exports.handler = func
+  return {
+    body: JSON.stringify(response),
+    statusCode: 200,
+  };
+};
 
+exports.handler = func;
